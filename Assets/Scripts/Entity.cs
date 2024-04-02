ae0e2e1b3c9cb6b7e63ffde0fdd6b5108cb43bd3
Suncrest45 <aidan.straker@gmail.com>
2024-04-02 01:52:23 -0700

    More Inheritance

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -7,33 +7,59 @@ public class Entity : MonoBehaviour
     //------------------------------------------------------------------------------------------------------------------------------------
     // Member Variables
     //------------------------------------------------------------------------------------------------------------------------------------
+
+    // Name of the entity
+    public string EntityName = string.Empty;
     // Max Health of the Entity
     public float MaxHitPoints = 200.0f;
-    // Max Resource of the Entity
-    public float MaxPower = 100.0f;
     // How quickly the entity moves
     public float MoveSpeed = 0.1f;
     // The optimal distance the AI will try to maintain from targets
     public float OptimalRange = 5.0f;
+    // Stun timer for an entity
+    public float StunTimer = 0.0f;
 
-    [HideInInspector]
-    private float HitPoints = 200; // Current hit points
-    [HideInInspector]
-    private float Power = 40;      // Current power
-    [HideInInspector]
-    private Entity Target;         // Current Target
+    // Damage taken during a round
+    private float DamageTaken = 0.0f;
+    // Damage dealt during a round
+    private float DamageDealt = 0.0f;
+
+    // Current hit points
+    private float HitPoints = 200;
+
+    // Current Target
+    private Entity Target;         
+
+    // Reference to the healthbar
+    private BarScaler HealthBar;
+
+    // An array of abilities for this entity
+    public Ability[] Abilities;
 
 
+    // Moves the entity
+    public virtual void DoMovement() { }
 
-    // Start is called before the first frame update
-    void Start()
+    // Initialises the entity
+    public virtual void Initialise() { }
+
+    // Finds the entities target
+    public virtual Entity FindTarget() { return null; }
+
+    // Use a randomly chosen ability
+    public bool UseRandomAbility() { return UseAbility(Random.Range(0, Abilities.Length)); }
+
+    // Use a specific ability
+    public bool UseAbility(int abilityNumber)
     {
-        
-    }
+        if (abilityNumber < Abilities.Length)
+        {
+            if (Abilities[abilityNumber] != null)
+            {
+                return Abilities[abilityNumber].Use();
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return false;
     }
 }
